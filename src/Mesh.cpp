1f616cf1f42ad5ab3911ae38e3b6628900a9b6f7
Rustam <afaritovich@mail.ru>
2020-04-17 12:26:19 +0700

    add: added realization for time-dependent function

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -24,15 +24,57 @@ size_t Mesh::GetSize()
     return mesh.size();
 }
 
-DiscreteFunction::DiscreteFunction() = default;
-DiscreteFunction::DiscreteFunction(const Mesh &mesh)
+double Mesh::operator[](size_t index) const
+{
+    return mesh[index];
+}
+
+double& Mesh::operator[](size_t index)
+{
+    return mesh[index];
+}
+
+SliceFunction::SliceFunction() = default;
+SliceFunction::SliceFunction(const Mesh &mesh)
 {
     this->mesh = std::make_shared<Mesh>(mesh);
     this->value = std::vector<double>(this->mesh->GetSize(), 0.0);
 }
 
-DiscreteFunction::DiscreteFunction(const std::shared_ptr<Mesh> &mesh)
+SliceFunction::SliceFunction(std::shared_ptr<Mesh> mesh)
 {
     this->mesh = mesh;
     this->value = std::vector<double>(mesh->GetSize(), 0.0);
+}
+
+SliceFunction::SliceFunction(const std::shared_ptr<Mesh> &mesh)
+{
+    this->mesh = mesh;
+    this->value = std::vector<double>(mesh->GetSize(), 0.0);
+}
+
+void SliceFunction::SetValue(std::function<double(double)> f)
+{
+    int index = 0;
+    for (auto &elem : this->value)
+    {
+        elem = f((*mesh)[index]);
+        ++index;
+    }
+}
+
+void Function::SetInitialValue(SliceFunction initial_value, double initial_time)
+{
+    if (data.size() != 0){
+        return;
+    }
+    data.emplace_back(initial_time, initial_value);
+}
+
+void Function::SetInitialValue(const SliceFunction &initial_value, double initial_time)
+{
+    if (data.size() != 0){
+        return;
+    }
+    data.push_back(std::pair(initial_time, initial_value));
 }
--- src/Mesh.hpp
+++ src/Mesh.hpp
@@ -2,6 +2,8 @@
 
 #include <vector>
 #include <memory>
+#include <functional>
+#include <utility>
 
 class Mesh
 {
@@ -13,18 +15,37 @@ public:
     Mesh(size_t points_number, double left, double right);
     void SetMesh(size_t points_number, double left, double right);
     size_t GetSize();
+    double operator[](size_t index) const;
+    double& operator[](size_t index);
 };
 
 
-class DiscreteFunction
+class SliceFunction
 {
 private:
     std::shared_ptr<Mesh> mesh;
     std::vector<double> value;
 
 public:
-    DiscreteFunction();
-    DiscreteFunction(const Mesh &mesh);
-    DiscreteFunction(const std::shared_ptr<Mesh> &mesh);
+    SliceFunction();
+    SliceFunction(const Mesh &mesh);
+    SliceFunction(std::shared_ptr<Mesh> mesh);
+    SliceFunction(const std::shared_ptr<Mesh> &mesh);
+
+    void SetValue(std::function<double(double)> f);
+};
+
+class Function
+{
+private:
+    /* Time + SliceFunction bbinding with time */
+    std::vector<std::pair<double, SliceFunction>> data;
+    std::shared_ptr<Mesh> mesh;
+
+public:
+    Function();
+
+    void SetInitialValue(SliceFunction initial_value, double initial_time = 0.0);
+    void SetInitialValue(const SliceFunction &initial_value, double initial_time = 0.0);
 
 };
